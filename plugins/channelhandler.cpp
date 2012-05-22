5a1d5cdcb994d8d6d08d741e3a2257ea49afbb88
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2012-05-22 12:31:19 -0700

    fix headers, copyrights and make it compile.

--- plugins/channelhandler.cpp
+++ plugins/channelhandler.cpp
@@ -1,9 +1,32 @@
+/*
+ * Copyright (C) 2012 Canonical, Ltd.
+ *
+ * Authors:
+ *  Tiago Salem Herrmann <tiago.herrmann@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "channelhandler.h"
+
+#include <TelepathyQt/CallChannel>
+
 ChannelHandler::ChannelHandler(const Tp::ChannelClassSpecList &channelFilter)
     : Tp::AbstractClientHandler(channelFilter)
 {
 }
 
-void ChannelHandler::bypassApproval() const
+bool ChannelHandler::bypassApproval() const
 {
     return false;
 }
@@ -18,7 +41,7 @@ void ChannelHandler::handleChannels(const Tp::MethodInvocationContextPtr<> &cont
 {
     foreach(const Tp::ChannelPtr &channel, channels) {
         QVariantMap properties = channel->immutableProperties();
-        if (properties[TP_QT_IFACE_CHANNEL ".ChannelType"] ==
+        if (properties[QString(TP_QT_IFACE_CHANNEL) + ".ChannelType"] ==
                 TP_QT_IFACE_CHANNEL_TYPE_CALL) {
             if (channel->isRequested()) {
             } else {
