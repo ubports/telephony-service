7d8c858defd6989179de9122842169bdb1a70edb
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2012-07-13 07:28:52 -0700

    add reference to the original source code

--- TelephonyApp/phoneutils.h
+++ TelephonyApp/phoneutils.h
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Original source code available at: http://androidxref.com/4.0.4/xref/frameworks/base/telephony/java/android/telephony/PhoneNumberUtils.java
  */
 
 #ifndef PHONENUMBERUTILS_H
