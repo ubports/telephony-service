5c2aec8d3f3a3019db05a07276a34741f42f32da
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2012-07-20 13:57:39 -0300

    Absolute image paths in snap decisions are handle through hints.

--- TelephonyAppApprover/telephonyappapprover.cpp
+++ TelephonyAppApprover/telephonyappapprover.cpp
@@ -227,9 +227,18 @@ void TelephonyAppApprover::onChannelReady(Tp::PendingOperation *op)
         }
     }
 
-    notification = notify_notification_new (title.toStdString().c_str(),
-                                            body.toStdString().c_str(),
+    if (icon != contact->avatarData().fileName) {
+        notification = notify_notification_new (title.toStdString().c_str(),
+                                                body.toStdString().c_str(),
+                                                icon.toStdString().c_str());
+    } else {
+        notification = notify_notification_new (title.toStdString().c_str(),
+                                                body.toStdString().c_str(),
+                                                NULL);
+        notify_notification_set_hint_string(notification,
+                                            "image_path",
                                             icon.toStdString().c_str());
+    }
     notify_notification_set_hint_string(notification,
                                         "x-canonical-snap-decisions",
                                         "true");
