b6e816200f2816e8eabe51def8ebe38526b6865b
Kaleo <florian.boucault@canonical.com>
2012-10-29 18:20:24 +0100

    Removed hack resizing the view.

--- telephonyapplication.cpp
+++ telephonyapplication.cpp
@@ -89,13 +89,6 @@ bool TelephonyApplication::setup()
     m_view->setSource(source);
     m_view->show();
 
-    // FIXME: remove this once the resolution independency stuff gets in.
-    if (singlePanel) {
-        m_view->resize(720,1280);
-    } else {
-        m_view->resize(1280,1280);
-    }
-
     QObject::connect(m_dbus, SIGNAL(request(QString)), this, SLOT(onMessageReceived(QString)));
     return true;
 }
