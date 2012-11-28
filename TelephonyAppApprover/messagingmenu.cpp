30c667846dcc83d0ac0a0c50be61a00acbe2122b
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2012-11-28 15:51:06 -0200

    Fix the displaying of icons and add support for showing missed calls in the messaging menu.

--- TelephonyAppApprover/messagingmenu.cpp
+++ TelephonyAppApprover/messagingmenu.cpp
@@ -1,6 +1,7 @@
 #include "messagingmenu.h"
 #include "contactmodel.h"
 #include "contactentry.h"
+#include <gio/gio.h>
 
 MessagingMenu::MessagingMenu(QObject *parent) :
     QObject(parent)
@@ -8,8 +9,7 @@ MessagingMenu::MessagingMenu(QObject *parent) :
     // create the messaging menu app
     mApp = messaging_menu_app_new("telephony-app.desktop");
     messaging_menu_app_register(mApp);
-    // TODO: add icon and check label
-    messaging_menu_app_append_source(mApp, "telephony-app", NULL, "Telephony App");
+    messaging_menu_app_append_source(mApp, "telephony-app", g_icon_new_for_string("telephony-app", NULL), "Telephony App");
 }
 
 void MessagingMenu::addMessage(const QString &phoneNumber, const QString &messageId, const QDateTime &timestamp, const QString &text)
@@ -20,23 +20,25 @@ void MessagingMenu::addMessage(const QString &phoneNumber, const QString &messag
     QString contactAlias = phoneNumber;
 
     if (contact) {
-        icon = contact->avatar().path();
+        icon = contact->avatar().toLocalFile();
         contactAlias = contact->displayLabel();
     }
 
-    MessagingMenuMessage *message = messaging_menu_message_new(messageId.toLocal8Bit().data(),
+    GFile *file = g_file_new_for_path(icon.toUtf8().data());
+    MessagingMenuMessage *message = messaging_menu_message_new(messageId.toUtf8().data(),
+                                                               g_file_icon_new(file),
+                                                               contactAlias.toUtf8().data(),
                                                                NULL,
-                                                               contactAlias.toLocal8Bit().data(),
-                                                               NULL,
-                                                               text.toLocal8Bit().data(),
+                                                               text.toUtf8().data(),
                                                                timestamp.toMSecsSinceEpoch());
+    g_object_unref(file);
     mMessages[messageId] = message;
     messaging_menu_app_append_message(mApp, message, "telephony-app", true);
+    // TODO: setup callbacks
 }
 
 void MessagingMenu::removeMessage(const QString &messageId)
 {
-    qDebug() << "Going to remove message" << messageId;
     if (!mMessages.contains(messageId)) {
         return;
     }
@@ -44,6 +46,55 @@ void MessagingMenu::removeMessage(const QString &messageId)
     MessagingMenuMessage *message = mMessages.take(messageId);
     messaging_menu_app_remove_message(mApp, message);
     g_object_unref(message);
+}
+
+void MessagingMenu::addCall(const QString &phoneNumber, const QDateTime &timestamp)
+{
+    Call call;
+    bool found = false;
+    Q_FOREACH(Call callMessage, mCalls) {
+        if (ContactModel::comparePhoneNumbers(callMessage.number, phoneNumber)) {
+            call = callMessage;
+            found = true;
+            mCalls.removeOne(callMessage);
+
+            // remove the previous entry and add a new one increasing the missed call count
+            messaging_menu_app_remove_message(mApp, call.message);
+            g_object_unref(call.message);
+            break;
+        }
+    }
+
+    if (!found) {
+            ContactEntry *contact = ContactModel::instance()->contactFromPhoneNumber(phoneNumber);
+            if (contact) {
+                call.contactAlias = contact->displayLabel();
+                call.contactIcon = contact->avatar().toLocalFile();
+            } else {
+                call.contactAlias = phoneNumber;
+            }
+            call.number = phoneNumber;
+            call.count = 0;
+    }
+
+    call.count++;
+
+    QString text;
+    if (call.count > 1) {
+        text = QString("%1 missed calls").arg(call.count);
+    } else {
+        text = QString("1 missed call");
+    }
+    GFile *file = g_file_new_for_path(call.contactIcon.toUtf8().data());
+    call.message = messaging_menu_message_new(call.number.toUtf8().data(),
+                                              g_file_icon_new(file),
+                                              call.contactAlias.toUtf8().data(),
+                                              NULL,
+                                              text.toUtf8().data(),
+                                              timestamp.toMSecsSinceEpoch());
+    messaging_menu_app_append_message(mApp, call.message, "telephony-app", true);
+    mCalls.append(call);
+    // TODO: setup actions and callbacks
 }
 
 
--- TelephonyAppApprover/messagingmenu.h
+++ TelephonyAppApprover/messagingmenu.h
@@ -6,6 +6,21 @@
 #include <messaging-menu.h>
 #include <messaging-menu-message.h>
 
+class Call
+{
+public:
+    Call() : count(0), message(0) { }
+    QString number;
+    int count;
+    QString contactAlias;
+    QString contactIcon;
+    MessagingMenuMessage *message;
+
+    bool operator==(const Call &other) {
+        return other.number == number;
+    }
+};
+
 class MessagingMenu : public QObject
 {
     Q_OBJECT
@@ -16,11 +31,14 @@ public:
     void addMessage(const QString &phoneNumber, const QString &messageId, const QDateTime &timestamp, const QString &text);
     void removeMessage(const QString &messageId);
 
+    void addCall(const QString &phoneNumber, const QDateTime &timestamp);
+
 private:
     explicit MessagingMenu(QObject *parent = 0);
 
     MessagingMenuApp *mApp;
     QMap<QString, MessagingMenuMessage*> mMessages;
+    QList<Call> mCalls;
 };
 
 #endif // MESSAGINGMENU_H
