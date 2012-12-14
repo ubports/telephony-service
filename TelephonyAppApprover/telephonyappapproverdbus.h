2a72c165b65e59b5a5ca795d2891c7efa4257207
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2012-12-14 19:33:16 -0200

    another credit

--- TelephonyAppApprover/telephonyappapproverdbus.h
+++ TelephonyAppApprover/telephonyappapproverdbus.h
@@ -3,6 +3,7 @@
  *
  * Authors:
  *  Ugo Riboni <ugo.riboni@canonical.com>
+ *  Tiago Salem Herrmann <tiago.herrmann@canonical.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
