09bf51738f2e04739830bae0b43051d8654f8115
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2013-01-09 16:28:15 -0200

    Remove the test_call test as we can't really evaluate the results, and the input field is not cleared when pressing the call button anymore: it waits until the live call view is displayed before cleaning it.

--- tests/autopilot/telephony_app/tests/test_call_panel.py
+++ tests/autopilot/telephony_app/tests/test_call_panel.py
@@ -61,31 +61,6 @@ class TestCallPanel(TelephonyAppTestCase):
         self.pointing_device.click()
         self.assertThat(dial_button.enabled, Eventually(Equals(False)))
 
-    def test_call(self):
-        """Dialing a number works"""
-        keypad_entry = self.call_panel.get_keypad_entry()
-        keypad_keys = self.call_panel.get_keypad_keys()
-
-        self.pointing_device.move_to_object(keypad_keys[0])
-        self.pointing_device.click()
-        self.pointing_device.move_to_object(keypad_keys[1])
-        self.pointing_device.click()
-        self.pointing_device.move_to_object(keypad_keys[2])
-        self.pointing_device.click()
-        self.pointing_device.move_to_object(keypad_keys[3])
-        self.pointing_device.click()
-
-        self.assertThat(keypad_entry.value, Eventually(Equals("1234")))
-        
-        dial_button = self.call_panel.get_dial_button()
-        self.pointing_device.move_to_object(dial_button)
-        self.pointing_device.click()
-        
-        # Hmpf... Nothing to evaluate right now... Lets just make sure the input
-        # field cleared itself for now
-        keypad_entry = self.call_panel.get_keypad_entry()
-        self.assertThat(keypad_entry.value, Eventually(Equals("")))
-
     def test_switch_to_contacts(self):
         """Switching to the contact list using the dialers contacts button"""
         dialer_page = self.call_panel.get_dialer_page()
