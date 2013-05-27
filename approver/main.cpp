2bd80dd82e0665f38ab4d508e391915a9959d849
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2013-05-27 15:57:45 -0300

    Added translatior for approver.

--- approver/main.cpp
+++ approver/main.cpp
@@ -33,9 +33,17 @@
 #include <TelepathyQt/AccountManager>
 #include <TelepathyQt/Contact>
 
+namespace C {
+#include <libintl.h>
+#define _(STRING)    gettext(STRING)
+}
+
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
+
+    C::bindtextdomain( "phone-app", "/usr/share/locale" );
+    C::textdomain("phone-app");
 
     notify_init("Phone App Approver");
 
