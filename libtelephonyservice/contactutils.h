fd7a131c1390a40faff997ac1ba69322ae209120
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2013-07-31 18:27:46 -0300

    Add missing copyright headers.

--- libtelephonyservice/contactutils.h
+++ libtelephonyservice/contactutils.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *  Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
+ *
+ * This file is part of telephony-service.
+ *
+ * telephony-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * telephony-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CONTACTUTILS_H
 #define CONTACTUTILS_H
 
