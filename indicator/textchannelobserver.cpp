ea59483c84e7312430f12fe1c1bfc43e903e24bf
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2013-09-25 18:27:28 -0300

    Remove leftover return statement.

--- indicator/textchannelobserver.cpp
+++ indicator/textchannelobserver.cpp
@@ -103,7 +103,6 @@ void TextChannelObserver::showNotificationForMessage(const Tp::ReceivedMessage &
 
     request->setManager(ContactUtils::sharedManager());
     request->start();
-    return;
 }
 
 Tp::TextChannelPtr TextChannelObserver::channelFromPath(const QString &path)
