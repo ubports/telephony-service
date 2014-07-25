51408b6007f484112244247c6e5e6586cc49ac91
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2014-07-25 14:32:04 -0300

    Update the accounts displayName according to a map (later it will be according to what is in systemsettings).

---
+++ handler/displaynamesettings.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *  Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
+ *
+ * This file is part of telephony-service.
+ *
+ * telephony-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * telephony-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DISPLAYNAMESETTINGS_H
+#define DISPLAYNAMESETTINGS_H
+
+#include <QObject>
+#include <QMap>
+
+class DisplayNameSettings : public QObject
+{
+    Q_OBJECT
+public:
+    static DisplayNameSettings *instance();
+
+private Q_SLOTS:
+    void onAccountsChanged();
+
+private:
+    explicit DisplayNameSettings(QObject *parent = 0);
+    QMap<QString,QString> mAccountNames;
+
+};
+
+#endif // DISPLAYNAMESETTINGS_H
