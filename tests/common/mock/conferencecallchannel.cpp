c808d741e0aa2c1cb64a83b2d067d7a2cbc85290
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2015-06-16 12:59:34 -0300

    update to telepathy-qt 0.9.6.1

--- tests/common/mock/conferencecallchannel.cpp
+++ tests/common/mock/conferencecallchannel.cpp
@@ -36,7 +36,7 @@ MockConferenceCallChannel::MockConferenceCallChannel(MockConnection *conn, QList
         }
     }
 
-    Tp::BaseChannelPtr baseChannel = Tp::BaseChannel::create(mConnection, TP_QT_IFACE_CHANNEL_TYPE_CALL, 0, Tp::HandleTypeNone);
+    Tp::BaseChannelPtr baseChannel = Tp::BaseChannel::create(mConnection, TP_QT_IFACE_CHANNEL_TYPE_CALL, Tp::HandleTypeNone, 0);
     Tp::BaseChannelCallTypePtr callType = Tp::BaseChannelCallType::create(baseChannel.data(),
                                                                           true,
                                                                           Tp::StreamTransportTypeUnknown,
