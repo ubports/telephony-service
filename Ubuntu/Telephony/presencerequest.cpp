38ba57be569fb891913276563210782d76bf6f64
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2015-06-25 12:14:57 -0300

    Implement PresenceRequest qml component

---
+++ Ubuntu/Telephony/presencerequest.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2013-2015 Canonical, Ltd.
+ *
+ * Authors:
+ *  Tiago Salem Herrmann <tiago.herrmann@canonical.com>
+ *
+ * This file is part of telephony-service.
+ *
+ * telephony-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * telephony-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "presencerequest.h"
+#include "telepathyhelper.h"
+
+#include <TelepathyQt/ContactManager>
+#include <TelepathyQt/PendingContacts>
+
+PresenceRequest::PresenceRequest(QObject *parent) :
+    QObject(parent), mCompleted(false)
+{
+}
+
+PresenceRequest::~PresenceRequest()
+{
+}
+
+void PresenceRequest::startPresenceRequest()
+{
+    if (!mCompleted || mIdentifier.isEmpty() || mAccountId.isEmpty()) {
+        // componenty is not ready yet
+        return;
+    }
+
+    AccountEntry *account = TelepathyHelper::instance()->accountForId(mAccountId);
+    if (!account || account->account()->connection().isNull()) {
+        return;
+    }
+ 
+    Tp::ContactManagerPtr contactManager = account->account()->connection()->contactManager();
+    Tp::PendingContacts *pendingContact = contactManager->contactsForIdentifiers(QStringList() << mIdentifier);
+    connect(pendingContact,
+                SIGNAL(finished(Tp::PendingOperation*)),
+                SLOT(onContactReceived(Tp::PendingOperation*)));
+}
+
+void PresenceRequest::onContactReceived(Tp::PendingOperation *op)
+{
+    Tp::PendingContacts *contacts = qobject_cast<Tp::PendingContacts *>(op);
+    if (!contacts || !contacts->isValid() || contacts->contacts().length() != 1) {
+        return;
+    }
+
+    if (mContact) {
+        disconnect(mContact.data(), 0,0,0);
+    }
+
+    mContact = contacts->contacts()[0];
+    connect(mContact.data(), SIGNAL(presenceChanged(const Tp::Presence &)), this, SLOT(onPresenceChanged()));
+    onPresenceChanged();
+}
+
+void PresenceRequest::onPresenceChanged()
+{
+    Q_EMIT statusChanged();
+    Q_EMIT statusMessageChanged();
+    Q_EMIT typeChanged();
+}
+
+uint PresenceRequest::type() const
+{
+    if (mContact) {
+        return mContact->presence().type();
+    }
+
+    return PresenceTypeUnset;
+}
+
+QString PresenceRequest::status() const
+{
+    if (mContact) {
+        return mContact->presence().status();
+    }
+    return QString();
+}
+
+QString PresenceRequest::statusMessage() const
+{
+    if (mContact) {
+        return mContact->presence().statusMessage();
+    }
+    return QString();
+}
+
+QString PresenceRequest::accountId() const
+{
+    return mAccountId;
+}
+
+QString PresenceRequest::identifier() const
+{
+    return mIdentifier;
+}
+
+void PresenceRequest::setIdentifier(const QString &identifier)
+{
+    if (mIdentifier == identifier) {
+        return;
+    }
+
+    mIdentifier = identifier;
+
+    startPresenceRequest();
+}
+
+void PresenceRequest::setAccountId(const QString &accountId)
+{
+    if (mAccountId == accountId) {
+        return;
+    }
+
+    mAccountId = accountId;
+
+    AccountEntry *account = TelepathyHelper::instance()->accountForId(accountId);
+    if (!account) {
+        return;
+    }
+
+    connect(account, SIGNAL(connectedChanged()), this, SLOT(startPresenceRequest()));
+
+    startPresenceRequest();
+}
+
+void PresenceRequest::classBegin()
+{
+}
+
+void PresenceRequest::componentComplete()
+{
+    mCompleted = true;
+    startPresenceRequest();
+}
+
---
+++ Ubuntu/Telephony/presencerequest.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2013-2015 Canonical, Ltd.
+ *
+ * Authors:
+ *  Tiago Salem Herrmann <tiago.herrmann@canonical.com>
+ *
+ * This file is part of telephony-service.
+ *
+ * telephony-service is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * telephony-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRESENCEREQUEST_H
+#define PRESENCEREQUEST_H
+
+#include <QObject>
+#include <QQmlParserStatus>
+#include <TelepathyQt/Presence>
+#include <TelepathyQt/PendingOperation>
+
+#include "accountentry.h"
+
+class PresenceRequest : public QObject, public QQmlParserStatus
+{
+    Q_OBJECT
+    Q_INTERFACES(QQmlParserStatus)
+
+    Q_ENUMS(PresenceType)
+    Q_PROPERTY(QString identifier READ identifier WRITE setIdentifier NOTIFY identifierChanged)
+    Q_PROPERTY(QString accountId READ accountId WRITE setAccountId NOTIFY accountIdChanged)
+    Q_PROPERTY(QString status READ status NOTIFY statusChanged)
+    Q_PROPERTY(QString statusMessage READ statusMessage NOTIFY statusMessageChanged)
+    Q_PROPERTY(uint type READ type NOTIFY typeChanged)
+
+public:
+
+    enum PresenceType {
+        PresenceTypeUnset = Tp::ConnectionPresenceTypeUnset,
+        PresenceTypeOffline = Tp::ConnectionPresenceTypeOffline,
+        PresenceTypeAvailable = Tp::ConnectionPresenceTypeAvailable,
+        PresenceTypeAway = Tp::ConnectionPresenceTypeAway,
+        PresenceTypeExtendedAway = Tp::ConnectionPresenceTypeExtendedAway,
+        PresenceTypeHidden = Tp::ConnectionPresenceTypeHidden,
+        PresenceTypeBusy = Tp::ConnectionPresenceTypeBusy,
+        PresenceTypeUnknown = Tp::ConnectionPresenceTypeUnknown,
+        PresenceTypeError = Tp::ConnectionPresenceTypeError
+    };
+
+    explicit PresenceRequest(QObject *parent = 0);
+    ~PresenceRequest();
+
+    uint type() const;
+    QString status() const;
+    QString statusMessage() const;
+
+    QString identifier() const;
+    void setIdentifier(const QString &identifier);
+
+    QString accountId() const;
+    void setAccountId(const QString &accountId);
+
+    void classBegin();
+    void componentComplete();
+
+private Q_SLOTS:
+    void startPresenceRequest();
+    void onPresenceChanged();
+    void onContactReceived(Tp::PendingOperation *op);
+
+Q_SIGNALS:
+    void identifierChanged();
+    void accountIdChanged();
+    void statusChanged();
+    void statusMessageChanged();
+    void typeChanged();
+
+private:
+    void startSearching();
+
+    QString mIdentifier;
+    QString mAccountId;
+    bool mCompleted;
+    Tp::ContactPtr mContact;
+};
+
+#endif // PRESENCEREQUEST_H
