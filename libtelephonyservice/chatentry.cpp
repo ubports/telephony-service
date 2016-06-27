6b609fcd4e383c0e34ef0d5ed61ef83a9f6b508f
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2016-06-27 14:26:23 -0300

    notify ChatState's to qml

--- libtelephonyservice/chatentry.cpp
+++ libtelephonyservice/chatentry.cpp
@@ -305,6 +305,7 @@ void ChatEntry::addChannel(const Tp::TextChannelPtr &channel)
         ContactChatState *state = new ContactChatState(contact->id(), channel->chatState(contact));
         mChatStates[contact->id()] = state;
     }
+    Q_EMIT chatStatesChanged();
 
     // now fill the properties with the data from the channel
     if (chatType() != (ChatType)channel->targetHandleType()) {
