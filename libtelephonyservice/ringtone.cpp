768af41cd02eccf43292fbb7360684974a7a66ab
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2016-08-23 16:43:05 +0200

    Fix style issue

--- libtelephonyservice/ringtone.cpp
+++ libtelephonyservice/ringtone.cpp
@@ -74,8 +74,7 @@ void RingtoneWorker::playIncomingMessageSound()
 
     // Re-create if in error state. A typical case is when media-hub-server has
     // crashed and we need to start from a clean slate.
-    if (mMessageAudioPlayer && mMessageAudioPlayer->error())
-    {
+    if (mMessageAudioPlayer && mMessageAudioPlayer->error()) {
         qDebug() << "mMessageAudioPlayer in error state ("
                  << mMessageAudioPlayer->error() << "), recreating";
 
