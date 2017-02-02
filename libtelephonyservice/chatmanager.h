c23876e76fede473b7b6e44e730d60598ca767c8
Tiago Salem Herrmann <tiago.herrmann@canonical.com>
2017-02-02 17:53:42 -0200

    Allow leave single channel by properties

--- libtelephonyservice/chatmanager.h
+++ libtelephonyservice/chatmanager.h
@@ -55,6 +55,7 @@ public Q_SLOTS:
     void acknowledgeMessage(const QVariantMap &properties);
     void acknowledgeAllMessages(const QVariantMap &properties);
     void leaveRooms(const QString &accountId, const QString &message);
+    void leaveRoom(const QVariantMap &properties, const QString &message);
 
 private Q_SLOTS:
     void onChannelObserverUnregistered();
